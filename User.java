0f07466ce6e021e00b29c827d08627258d025ee3
Jennifer Ly <ly136@mail.chapman.edu>
2017-04-16 12:33:25 -0700

    added User class

---
+++ User.java
@@ -0,0 +1,68 @@
+class User{
+
+  String username;
+  String password;
+
+  User(){
+    this.username = "";
+    this.password = "";
+  }
+
+  User(String name, String pass){
+    this.username = name;
+    this.password = pass;
+  }
+
+  public void setUsername(String name){
+    this.username = name;
+  }
+
+  public void setPassword(String pass){
+    this.password = pass;
+  }
+
+  public String getUsername(){
+    return this.username;
+  }
+
+  public String getPassword(){
+    return this.password;
+  }
+
+  public boolean checkUserName(String name){
+
+    if (name.length() == this.username.length()){
+      for (int i = 0; i < this.username.length(); i++){
+        if (name[i] != this.username[i]){
+          // entry does not match
+          return false;
+        }
+      }
+
+      // every character matches up
+      // the length is the also the same
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public boolean checkPassword(String pass){
+
+    if (pass.length() == this.password.length()){
+      for (int i = 0; i < this.password.length(); i++){
+        if (pass[i] != this.password[i]){
+          return false;
+        }
+      }
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
+
+
+}
